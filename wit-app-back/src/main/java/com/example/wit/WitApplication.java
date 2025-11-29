a41b97479e6b55d0c38d97e3ae0344875ec410ad
ValVas17 <valeria.vasilyeva@yandex.ru>
2025-11-29 22:33:18 +0300

    Пересобрала бэк

--- wit-app-back/src/main/java/com/example/wit/WitApplication.java
+++ wit-app-back/src/main/java/com/example/wit/WitApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.wit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WitApplication.class, args);
 	}
 
 }
